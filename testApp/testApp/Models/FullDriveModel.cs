2d81b1e4f66ab8d8406ce5459b0737d14d8bccaf
Medvedevsky <killumexxx@gmail.com>
2022-12-18 17:03:39 +0300

    feat: FullDrive model

---
+++ testApp/testApp/Models/FullDriveModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApp.Models
+{
+    public record class FullDriveModel(string DriveName, double AvailableFreeSpaceGb, double TotalSizeSpaceGb, double Percent);
+}
