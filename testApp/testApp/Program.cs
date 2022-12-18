1b8331de4f33ce43df12fc9042c70dbe8358f748
Medvedevsky <killumexxx@gmail.com>
2022-12-18 16:51:15 +0300

    feat: Kernel32 dll

--- testApp/testApp/Program.cs
+++ testApp/testApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using testApp;
+
+Kernel32.GetDiskFreeSpaceEx("C:\\", out ulong lpFreeBytesAvailable,
+                                    out ulong lpTotalNumberOfBytes,
+                                    out ulong lpTotalNumberOfFreeBytes);
+
+var avalivaleGb = lpFreeBytesAvailable / 1073741824;
+var totalNumberGb = lpTotalNumberOfBytes / 1073741824;
+var totalNumberOfFreeBytes = lpTotalNumberOfFreeBytes / 1073741824;
+
+Console.WriteLine($"Доступно: {avalivaleGb}");
+Console.WriteLine($"Всего: {totalNumberGb}");
+
