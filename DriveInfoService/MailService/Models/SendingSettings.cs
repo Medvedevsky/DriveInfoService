3f784eb66ac5bbc26bdc29fcd74e1c99faf6fd40
Medvedevsky <killumexxx@gmail.com>
2022-12-19 23:22:44 +0300

    feat: model of configuration smtp-client

---
+++ DriveInfoService/MailService/Models/SendingSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailService.Models
+{
+    public class SendingSettings
+    {
+        public string SenderEmail { get; set; }
+        public string SenderEmailPassword { get; set; }
+        public int SenderPort { get; set; }
+        public string SenderHost { get; set; }
+        public string Message { get; set; }
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+    }
+}
