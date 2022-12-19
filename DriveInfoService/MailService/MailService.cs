aa120a53248d6e57921d660eb9c5ac5701b0a5fb
Medvedevsky <killumexxx@gmail.com>
2022-12-19 23:22:59 +0300

    feat: add method of sending emails

---
+++ DriveInfoService/MailService/MailService.cs
@@ -0,0 +1,47 @@
+﻿using MailKit.Net.Smtp;
+using MailService.Models;
+using MimeKit;
+
+namespace MailService
+{
+    public static class MailService
+    {
+        public static async Task<bool> SendEmailAsync(SendingSettings settings, string recipientEmail)
+        {
+            using SmtpClient client = new();
+
+            await client.ConnectAsync(settings.SenderHost, settings.SenderPort, false);
+            await client.AuthenticateAsync(settings.SenderEmail, settings.SenderEmailPassword);
+
+            try
+            {
+                MimeMessage emailMessage = new();
+                emailMessage.From.Add(new MailboxAddress(settings.Sender, settings.SenderEmail));
+                emailMessage.To.Add(MailboxAddress.Parse(recipientEmail));
+                emailMessage.Subject = settings.Subject;
+            
+                BodyBuilder builder = new();
+            
+                if (string.IsNullOrWhiteSpace(settings.Message) == false)
+                {
+                    builder.TextBody = settings.Message;
+                }
+            
+                emailMessage.Body = builder.ToMessageBody();
+            
+            
+                await client.SendAsync(emailMessage);
+                
+                //logger.Trace($"Сообщение отправлено, получатель '{count}' '{email}' из '{emailCount}'");
+            }
+            catch (Exception ex)
+            {
+                //logger.Error($"Произошла ошибка при отправке сообщения: {ex}");
+                return false;
+            }
+            
+            await client.DisconnectAsync(true);
+            return true;
+        }
+    }
+}
